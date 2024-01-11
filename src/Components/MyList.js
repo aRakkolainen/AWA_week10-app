a1478e658ae5e025b71fd1e5d233792b07f89065
aRakkolainen <aino.rakkolainen@gmail.com>
2024-01-11 19:26:48 +0200

    Task 3?

--- src/Components/MyList.js
+++ src/Components/MyList.js
@@ -1,8 +1,18 @@
 //import React from "react";
 
 
-function MyList(header, items) {
-    console.log(items.items);
+function MyList(props) {
+    //console.log(props.header)
+    //console.log(props.items);
+    const items = props.items; 
+    const listItems = items.map((item) => 
+    <li>{item.text}</li>)
+    return (
+        <>
+            <h1>{props.header}</h1>
+            <ul>{listItems}</ul>
+        </>
+    )
 }
 
 export default MyList;
